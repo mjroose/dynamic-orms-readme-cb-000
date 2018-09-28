65e1b7b5a8f329b90fc33799e940042d7b433ae0
Michael Roose <mjroose@gmail.com>
2018-09-28 19:56:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
   end
 
   def values_for_insert
-    values = self.class.column_names.collect do |col_name|
+    self.class.column_names.collect do |col_name|
       send(col_name).nil? ? nil : "'#{send(col_name)}'"
     end.compact.join(", ")
   end
