5d1c0a53954b293e097fa97b71907973942553db
Michael Roose <mjroose@gmail.com>
2018-09-28 19:49:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,11 +14,10 @@ class Song
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
-    column_names = []
-    table_info.each do |row|
-      column_names << row["name"]
-    end
-    column_names.compact
+
+    column_names = table_info.collect do |row|
+      row["name"]
+    end.compact
     binding.pry
   end
 
